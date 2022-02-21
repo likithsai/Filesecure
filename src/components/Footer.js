b225fad7c7821cc94cbb935ea7c6c100b9a658bb
likithsai <likithsai13@gmail.com>
2022-02-21 20:29:16 +0530

    minor code changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-        <footer className='border-top'>
+        <footer className='border-top fixed-bottom bg-white'>
             <div className="container my-4 text-center  ">
                 <p className='mb-2 text-muted'>{ props.text }</p>
             </div>
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,7 +3,7 @@ import React from "react";
 const TextArea = (props) => {
     return (
         <div class="form-floating">
-            <textarea class="form-control" placeholder={props.placeholder} id={ props.key } style={{ height: '100px' }} onChange={props.onChange}>{ props.value }</textarea>
+            <textarea class="form-control" placeholder={props.placeholder} id={ props.key } onChange={props.onChange}>{ props.value }</textarea>
             <label for="floatingTextarea">{props.title}</label>
         </div>
     );
